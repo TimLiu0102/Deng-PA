8d72692f63fe8920409bf2ad0a2518daaa8af1e1
TimLiu0102 <2258392473@qq.com>
2026-04-02 17:37:25 +0800

    Fix free-space channel formulas to match paper

--- main.m
+++ main.m
@@ -30,7 +30,8 @@ params.a = 10;
 params.b = 6;
 params.v = 1.1;
 params.n_refr = 1.5;
-params.eta = 1.0;
+% 对应论文自由空间传播常数公式：eta = lambda^2 / (4*pi)
+params.eta = params.lambda^2 / (4*pi);
 params.P_max = 1.0;
 params.sigma2 = 1e-3;
 
