8775a5f071a6708138241155ce8630c122aeecd8
TimLiu0102 <2258392473@qq.com>
2026-04-17 01:00:15 +0800

    可切换随机/初始化+X/穷搜X

--- main.m
+++ main.m
@@ -136,10 +136,10 @@ for t = 1:params.T_max
     R_after_angle = Signal_model('sum_rate', params, scene, state, []);
 
     % 3) 更新位置
-    [state.X, DEBUG_X_t] = AO_X(params, scene, model, state);
-    history.X_update_mode = 'gradient';
-    % [state.X, DEBUG_X_t] = AO_X_ex(params, scene, model, state);
-    % history.X_update_mode = 'exhaustive';
+    % [state.X, DEBUG_X_t] = AO_X(params, scene, model, state);
+    % history.X_update_mode = 'gradient';
+    [state.X, DEBUG_X_t] = AO_X_ex(params, scene, model, state);
+    history.X_update_mode = 'exhaustive';
     R_after_X = Signal_model('sum_rate', params, scene, state, []);
 
     % 4) 更新用户集合
