bf0288fb63cffa012f0729093f9a2b0bc4416422
TimLiu0102 <2258392473@qq.com>
2026-05-19 03:51:37 +0800

    重新画图

--- Plot_Compare.m
+++ Plot_Compare.m
@@ -6,7 +6,7 @@ if nargin < 2 || isempty(base_scene)
     base_scene = Channel_model('build_scene', base_params, [], [], []);
 end
 
-plot_mode = 'full';   % 'debug' 或 'full'
+plot_mode = 'debug';   % 'debug' 或 'full'
 % debug 模式只减少 MC，不减少横轴取值；如果调试 PSO 较慢，可手动关闭 do_N/do_Dy。
 
 do_snr         = false;
@@ -15,9 +15,9 @@ do_N           = false;
 do_M           = false;
 do_Dy          = false;
 do_convergence = false;
-do_cdf         = true;
+do_cdf         = false;
 do_final_bar_ab = false;
-do_H2_ab = false;
+do_H2_ab = true;
 do_default_geometry = false;
 do_default_check = false;
 
@@ -58,7 +58,7 @@ else
 end
 
 if strcmp(plot_mode, 'debug')
-    MC = 3;
+    MC = 1;
 else
     MC = 300;
 end
